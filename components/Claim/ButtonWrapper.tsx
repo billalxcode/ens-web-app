8c387b6af81ba8da436edc1236bf522801e06291
billalxcode <billal.xcode@gmail.com>
2024-05-27 19:10:01 +0700

    (button): add gradient color

--- components/Claim/ButtonWrapper.tsx
+++ components/Claim/ButtonWrapper.tsx
@@ -1,15 +1,18 @@
 'use client';
-import { Button } from '@chakra-ui/react';
+import ButtonWrapperProps from '@/interface/props/ButtonWrapperProps';
+import { Button, Skeleton } from '@chakra-ui/react';
 import {
 	useWeb3Modal,
 	useWeb3ModalAccount,
 	useWeb3ModalProvider
 } from '@web3modal/ethers/react';
+import { Eip1193Provider } from 'ethers';
 import { BigNumberish } from 'ethers';
 import { BrowserProvider, ethers } from 'ethers';
 import React, { useEffect, useState } from 'react';
 
-export default function ButtonWrapper() {
+export default function ButtonWrapper(props: ButtonWrapperProps) {
+	const [isLoading, setIsLoading] = useState(true);
 	const { open: openModal } = useWeb3Modal();
 	const { isConnected, address: walletAddress } = useWeb3ModalAccount();
 	const { walletProvider } = useWeb3ModalProvider();
@@ -19,72 +22,88 @@ export default function ButtonWrapper() {
 	useEffect(() => {
 		(async () => {
 			if (isConnected) {
-				const provider = new BrowserProvider(walletProvider as any);
+				const provider = new BrowserProvider(
+					walletProvider as Eip1193Provider
+				);
 
 				const balance = await provider.getBalance(walletAddress || '');
 				setWalletBalance(balance);
 			}
+			setIsLoading(false);
 		})();
-	}, [isConnected, walletAddress, walletProvider]);
+	}, [isConnected, walletAddress, walletProvider, isLoading]);
 
-	if (!isConnected) {
-		return (
-			<Button
-				w={'full'}
-				mt={10}
-				p={7}
-				bgColor={'bg.button.primary'}
-				transition={'all .5s ease-in-out'}
-				_hover={{
-					bgColor: 'bg.button.hover.primary'
-				}}
-				_active={{
-					bgColor: 'bg.button.active.primary'
-				}}
-				onClick={() => openModal()}
-			>
-				Connect Wallet
-			</Button>
-		);
-	}
+	const renderContent = () => {
+		if (!isLoading && !isConnected) {
+			return (
+				<Button
+					w={'full'}
+					p={7}
+					bgColor={'bg.button.primary'}
+					transition={'all .5s ease-in-out'}
+					_hover={{
+						bgColor: 'bg.button.hover.primary'
+					}}
+					_active={{
+						bgColor: 'bg.button.active.primary'
+					}}
+					onClick={() => openModal()}
+				>
+					Connect Wallet
+				</Button>
+			);
+		} else {
+			if (parseFloat(ethers.formatEther(walletBalance)) == 0) {
+				return (
+					<Button
+						w={'full'}
+						p={7}
+						bgColor={'bg.button.primary'}
+						transition={'all .5s ease-in-out'}
+						cursor={'default'}
+						_hover={{
+							bgColor: 'bg.button.disabled.primary'
+						}}
+						_disabled={{
+							bgColor: 'bg.button.disabled.primary'
+						}}
+						isDisabled
+					>
+						Insufficient Balance
+					</Button>
+				);
+			} else {
+				return (
+					<Button
+						w={'full'}
+						p={7}
+						transition={'all .5s ease-in-out'}
+						bgGradient={'linear(to-l, #8aa9f2, #9a76ff)'}
+						bgSize={'100 100'}
+						_hover={{
+							transform: 'translateY(-5px)'
+						}}
+						_active={{
+							bgGradient: 'linear(to-l, #8aa9f2, #9a76ff)'
+						}}
+						onClick={() => props.setStep('commit')}
+					>
+						Next
+					</Button>
+				);
+			}
+		}
+	};
 
-	if (parseFloat(ethers.formatEther(walletBalance)) == 0) {
-		return (
-			<Button
-				w={'full'}
-				mt={10}
-				p={7}
-				bgColor={'bg.button.primary'}
-				transition={'all .5s ease-in-out'}
-				cursor={'default'}
-				_hover={{
-					bgColor: 'bg.button.disabled.primary'
-				}}
-				_disabled={{
-					bgColor: 'bg.button.disabled.primary'
-				}}
-				isDisabled
-			>
-				Insufficient Balance
-			</Button>
-		);
-	}
 	return (
-		<Button
-			w={'full'}
+		<Skeleton
 			mt={10}
-			p={7}
-			bgColor={'bg.button.primary'}
-			transition={'all .5s ease-in-out'}
-			_hover={{
-				bgColor: 'bg.button.hover.primary'
-			}}
-			_active={{
-				bgColor: 'bg.button.active.primary'
-			}}
-			onClick={() => openModal()}
+			isLoaded={!isLoading}
+			borderRadius={5}
+			startColor={'bg.skeletonStart'}
+			endColor={'bg.skeletonEnd'}
 		>
-			Next
-		</Button>
+			{renderContent()}
+		</Skeleton>
 	);
 }
