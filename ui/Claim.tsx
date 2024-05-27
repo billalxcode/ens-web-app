608c3820a65d4222faba15dceb73da2a8ebbd631
billalxcode <billal.xcode@gmail.com>
2024-05-27 19:12:51 +0700

    (ui): fix claim interface

--- ui/Claim.tsx
+++ ui/Claim.tsx
@@ -1,49 +1,38 @@
 'use client';
-import { Box, Divider, Flex, Heading, Skeleton, Text } from '@chakra-ui/react';
+import { Box, Flex, Heading, Text } from '@chakra-ui/react';
 import React, { useEffect, useState } from 'react';
-import Card from '../components/Card';
 import ClaimProps from '@/interface/props/ClaimProps';
-import RegistrationYearSelection from '@/components/Claim/RegistrationYearSelection';
-import estimateRegistration from '@/logic/estimateRegistration';
-import estimatedRegistrationStates from '@/interface/states/estimatedRegistration';
-import { ethers } from 'ethers';
-import ButtonWrapper from '@/components/Claim/ButtonWrapper';
-import { getGasPrice } from 'viem/actions';
-import client from '@/logic/client';
-import { BigNumberish } from 'ethers';
+import FormRegistration from '@/components/Claim/FormRegistration';
+import FormCommit from '@/components/Claim/FormCommit';
+import { useWeb3ModalAccount } from '@web3modal/ethers/react';
 
 export default function Claim(props: ClaimProps) {
-	const [year, setYear] = useState<number>(1);
-	const [estimatedRegistration, setEstimatedRegistration] =
-		useState<estimatedRegistrationStates>({
-			registrationFee: BigInt(0),
-			premiumFee: BigInt(0),
-			hasPremium: false
-		});
-	const [gasPrices, setGasPrices] = useState<BigNumberish>(0);
-	const [isEstimateRegistration, setIsEstimateRegistration] = useState(true);
+	const [step, setStep] = useState('registration');
+	const [durationInYear, setDurationInYear] = useState(1);
 
-	useEffect(() => {
-		(async () => {
-			if (isEstimateRegistration) {
-				const gasPrice = await getGasPrice(client);
-				console.log(gasPrice);
-				setGasPrices(gasPrice);
-				setTimeout(async () => {
-					const estimatedRegistrationData =
-						await estimateRegistration(props.params.username, year);
-					setEstimatedRegistration(estimatedRegistrationData);
+	const { address: ownerAddress } = useWeb3ModalAccount();
 
-					setIsEstimateRegistration(false);
-				}, 1000);
-			}
-		})();
-	}, [
-		year,
-		props.params.username,
-		estimatedRegistration,
-		isEstimateRegistration
-	]);
+	const renderContent = () => {
+		if (step == 'registration') {
+			return (
+				<FormRegistration
+					params={props.params}
+					setStep={(newStep: string) => setStep(newStep)}
+					setDurationInYear={(year) => setDurationInYear(year)}
+					durationInYear={durationInYear}
+				/>
+			);
+		} else if (step == 'commit') {
+			return (
+				<FormCommit
+					name={props.params.username}
+					owner={ownerAddress || ''}
+					duration={durationInYear}
+					setStep={(newStep) => setStep(newStep)}
+				/>
+			);
+		}
+	};
 
 	return (
 		<Box>
@@ -54,74 +43,15 @@ export default function Claim(props: ClaimProps) {
 					flexDirection={'column'}
 				>
 					<Box mb={30}>
-						<Heading mb={2} size={'lg'}>
+						<Heading mb={2} size={'lg'} textAlign={'center'}>
 							Register Domain Name
 						</Heading>
-						<Text>
+						<Text textAlign={'center'}>
 							You are registering available name:{' '}
 							{props.params.username}
 						</Text>
 					</Box>
-					<Card>
-						<RegistrationYearSelection
-							year={year}
-							setYear={(x) => setYear(x)}
-							setLoading={() => setIsEstimateRegistration(true)}
-						/>
-
-						<Divider opacity={0.5} my={3} />
-						<Flex flexDirection={'column'} gap={3}>
-							<Flex
-								justify={'space-between'}
-								flexDirection={'row'}
-							>
-								<Text>Registration Priod</Text>
-								<Text>{year} Year</Text>
-							</Flex>
-							<Flex
-								justify={'space-between'}
-								flexDirection={'row'}
-							>
-								<Text>Registration Fee</Text>
-								<Skeleton
-									isLoaded={!isEstimateRegistration}
-									borderRadius={5}
-									startColor={'bg.skeletonStart'}
-									endColor={'bg.skeletonEnd'}
-								>
-									<Text>
-										{ethers
-											.formatEther(
-												estimatedRegistration.registrationFee
-											)
-											.substring(0, 5)}{' '}
-										ETH
-									</Text>
-								</Skeleton>
-							</Flex>
-							<Flex
-								justify={'space-between'}
-								flexDirection={'row'}
-							>
-								<Text>Gas Price</Text>
-								<Skeleton
-									isLoaded={!isEstimateRegistration}
-									borderRadius={5}
-									startColor={'bg.skeletonStart'}
-									endColor={'bg.skeletonEnd'}
-								>
-									<Text>
-										{ethers
-											.formatUnits(gasPrices, 'gwei')
-											.substring(0, 5)}{' '}
-										GWEI
-									</Text>
-								</Skeleton>
-							</Flex>
-						</Flex>
-
-						<ButtonWrapper />
-					</Card>
+					{renderContent()}
 				</Flex>
 			</Flex>
 		</Box>
