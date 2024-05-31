55a7492f3aad085ac09e4489e5928f5935b671fe
billalxcode <billal.xcode@gmail.com>
2024-05-31 10:33:38 +0700

    (props): new form registration props

--- interface/props/FormRegistrationProps.ts
+++ interface/props/FormRegistrationProps.ts
@@ -3,5 +3,7 @@ import ClaimProps from './ClaimProps';
 export default interface FormRegistrationProps extends ClaimProps {
 	setStep: (newStep: string) => void;
 	setDurationInYear: (year: number) => void;
+	setIsPrimaryName: (checked: boolean) => void;
 	durationInYear: number;
+	isPrimaryName: boolean;
 }
