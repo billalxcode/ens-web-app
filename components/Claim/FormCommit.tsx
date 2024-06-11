3f37b19879318fa3eabdf6f52960a3ef9b3d4f29
billalxcode <billal.xcode@gmail.com>
2024-06-11 16:15:25 +0700

    add new toaster

--- components/Claim/FormCommit.tsx
+++ components/Claim/FormCommit.tsx
@@ -102,6 +102,10 @@ export default function FormCommit(props: FormCommitProps) {
 				resolve(hash);
 			} catch (e: any) {
 				if (e instanceof TransactionExecutionError) {
+					toast({
+						status: 'error',
+						title: e.shortMessage
+					})
 					reject(e.shortMessage);
 				}
 				reject(e.reason);
