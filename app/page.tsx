0427e8c11558d7e0ca4f97c45365e53738b3a0e9
billalxcode <billal.xcode@gmail.com>
2024-06-17 19:03:10 +0700

    remove floating action button

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@
 import Header from '../components/Header';
 import Home from '../ui/Home';
 import Footer from '../components/Footer';
-import FloationActionButton from '@/components/FloationActionButton';
 
 export default function Page() {
 	return (
